ddaa2afcf26e86e3539eec9bf1821f293849d070
zannetdev <randellcode@outlook.es>
2022-07-28 18:23:14 -0500

    Index Page Finished using Github Most Used Langs

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,27 @@ import {
   Box,
   Heading,
   Image,
+  List,
+  ListItem,
+  SimpleGrid,
+  Button,
   Link,
   useColorModeValue
 } from '@chakra-ui/react'
 import NextLink from 'next/link'
 import Section from '../components/section'
 import Paragraph from '../components/paragraph'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import { BioSection, BioYear } from '../components/bio'
+import { HamburgerIcon } from '@chakra-ui/icons'
+import { GridItem } from '../components/grid-item'
+import {
+  IoLogoTwitter,
+  IoLogoInstagram,
+  IoLogoGithub,
+  IoFileTrayStackedSharp
+} from 'react-icons/io5'
+
 const Page = () => {
   return (
     <Container>
@@ -57,11 +72,131 @@ const Page = () => {
           work in a team, he is very friendly and a technology enthusiast. He is
           not currently studying but in 2023 he will start his career at the
           University called
-          <NextLink href="https://www.ucem.edu.mx/" passHref>
-            <Link target="_blank"> UCEM </Link>
+          <NextLink
+            href="https://educem.mx/licenciaturas/licenciatura-en-ingenieria-informatica/"
+            passHref
+          >
+            <Link target="_blank"> EDUCEM </Link>
           </NextLink>
           specializing as a software engineer.
         </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/works">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="whatsapp">
+              My Portfolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>2002</BioYear>
+          Born in Guanajuato, México.
+        </BioSection>
+        <BioSection>
+          <BioYear>2018</BioYear>
+          Qualify to enter high school & I Started Studying Programation.
+        </BioSection>
+        <BioSection>
+          <BioYear>2020</BioYear>I worked together with
+          <NextLink href="https://www.cbtis65.edu.mx/" passHref>
+            <Link target="_blank"> CBTis 65 </Link>
+          </NextLink>
+          (My high school) to make an ERP for the same school.
+        </BioSection>
+        <BioSection>
+          <BioYear>2021</BioYear>I entered the university called
+          <NextLink href="https://irapuato.tecnm.mx/" passHref>
+            <Link target="_blank"> ITESI</Link>
+          </NextLink>
+          , but for reasons of time, finances and health I had to stop studying
+          for the time being.
+        </BioSection>
+        <BioSection>
+          <BioYear>2021 to Present</BioYear>
+          Working as Freelancer and Studying on Home.
+        </BioSection>
+      </Section>
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          I ♥
+        </Heading>
+        <Paragraph>
+          <Link href="https://steamcommunity.com/id/sixlegd" target="_blank">
+            VideoGames
+          </Link>
+          , Digital Content,&nbsp;
+          <Link href="https://www.instagram.com/gonedustx/" target="_blank">
+            Social Networks
+          </Link>
+          , Web Development and Machine Learning
+        </Paragraph>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          On the web
+        </Heading>
+        <List>
+          <ListItem>
+            <Link href="https://github.com/zannetdev" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoGithub />}
+              >
+                @zannetdev
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://twitter.com/ZannetCode" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoFileTrayStackedSharp />}
+              >
+                @gonedustx
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://twitter.com/ZannetCode" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoTwitter />}
+              >
+                @ZannetCode (Spanish)
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://instagram.com/gonedustx" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoInstagram />}
+              >
+                @gonedustx
+              </Button>
+            </Link>
+          </ListItem>
+        </List>
+
+        <SimpleGrid columns={[1]} gap={6}>
+          <GridItem href="https://github.com/zannetdev">
+            <p align="center">
+              <img
+                src="https://github-readme-stats.vercel.app/api/top-langs/?username=zannetdev&langs_count=10&theme=tokyonight&layout=compact"
+                alt="AnhellO :: Top Langs"
+              />
+            </p>
+          </GridItem>
+        </SimpleGrid>
       </Section>
     </Container>
   )
