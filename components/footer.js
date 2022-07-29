b345980628299ad9948c87c22907224eec51ac09
zannetdev <randellcode@outlook.es>
2022-07-28 19:53:57 -0500

    v1.0

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import { Box } from '@chakra-ui/react'
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
-      &copy; {new Date().getFullYear()} @zannetdev. All Rights Reserved.
+      &copy; {new Date().getFullYear()} @zannetdev. All Rights Reserved. | Credits to @Takuya Matsuyama
     </Box>
   )
 }
--- components/layouts/main.js
+++ components/layouts/main.js
@@ -14,7 +14,21 @@ const Main = ({ children, router }) => {
     <Box>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Zannet | Homepage</title>
+        <meta name="description" content="ZannetDev Homesite" />
+        <meta name="author" content="Eduardo González" />
+        <meta name="author" content="zannet" />
+        <link rel="apple-touch-icon" href="/contents/profile.png" />
+        <link rel="shortcut icon" href="/contents/profile.png" type="image/x-icon" />
+        <meta name="twitter:title" content="ZannetDev Homesite" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@ZannetCode" />
+        <meta name="twitter:creator" content="@ZannetCode" />
+        <meta name="twitter:image" content="https://user-images.githubusercontent.com/72852996/180954671-c531efaf-2bee-405e-bf66-d3510122a45f.png" />
+        <meta property="og:site_name" content="" />
+        <meta name="og:title" content="ZannetDev Homesite" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://user-images.githubusercontent.com/72852996/180954671-c531efaf-2bee-405e-bf66-d3510122a45f.png" />
+        <title>Zannet</title>
       </Head>
       <Navbar path={router.asPath} />
       <Container maxW="container.md" pt={14}>
